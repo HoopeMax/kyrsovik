54ce3970cdda7f5630558edbe9b69eeef2a96384
HoopeMax <nadezhda-maksimova-01@mail.ru>
2020-12-25 02:28:14 +0300

    создала новую втвь

--- kyrsovik/Source.cpp
+++ kyrsovik/Source.cpp
@@ -1,68 +1,97 @@
-#include<iostream>
+п»ї#include<iostream>
 #include <iomanip>
 #include<cmath>
-const int N=5;
-double eps = 0.001;
-//class point
-//{
-//public:
-//	
-//	
-//};
-//Решение СЛАУ методом итераций
-double* iter(double a[3][3], double y[3], int n)
+
+const double eps = 0.001;
+const int N = 5;
+const int ROWS = 3;
+
+class PointXY
 {
-	double* res = new double[n];
-	int i, j;
+private:
 
-
-	for (i = 0; i < n; i++)
+	double X;
+	double Y;
+	double Fi;
+public:
+	PointXY(double X, double Y)
 	{
-		res[i] = y[i] / a[i][i];
+		this->X = X;
+		this->Y = Y;
+		this->Fi = Fi;
 	}
 
-	double eps = 0.0001;
-	double* Xn = new double[n];
+	double GetX()
+	{
+		
+		return X;
+	}
 
-	do {
-		for (i = 0; i < n; i++) {
-			Xn[i] = y[i] / a[i][i];
-			for (j = 0; j < n; j++) {
-				if (i == j)
-					continue;
-				else {
-					Xn[i] -= a[i][j] / a[i][i] * res[j];
+	double SetX(double X)
+	{
+		this->X = X;
+
+	}
+	double GetY()
+	{
+
+		return Y;
+	}
+
+	double SetY(double Y)
+	{
+		this->Y = Y;
+
+	}
+	double CreatFi(double X[N], double Y[N], double Fi[ROWS][N])
+	{
+		for (int i = 0; i < 3; i++)
+		{
+			for (int j = 0; j < 5; j++)
+			{
+				if (i == 0)
+				{
+					Fi[i][j] = 1;
 				}
+
+				if (i == 1)
+				{
+					Fi[i][j] = X[j];
+				}
+
+				if (i == 2)
+				{
+					Fi[i][j] = ((3 * (X[j] * X[j])) - 1);
+				}
+
 			}
+
 		}
+		return Fi[ROWS][N];
+	}
+};
 
-		bool flag = true;
-		for (i = 0; i < n - 1; i++) {
-			if (abs(Xn[i] - res[i]) > eps) {
-				flag = false;
-				break;
-			}
-		}
 
-		for (i = 0; i < n; i++) {
-			res[i] = Xn[i];
-		}
 
-		if (flag)
-			break;
-	} while (1);
-
-	return res;
-}
-
+double* iter(double a[3][3], double y[3], int n);
 
 
 int main()
 {
 	setlocale(LC_ALL, "rus");
-	double X[N] = { -1.0 ,  -0.3 ,  0.4 ,  0.6 ,  1.0 };
-	double Y[N] = { 0.0 , 0.8 ,  0.7 ,  0.4 ,  0.1 };
-	double Fi[3][5];
+	BasisXY* XiYi = new BasisXY[N]
+	{
+		{-1.0, 0.0, },
+		{-0.3, 0.8, },
+		{ 0.4, 0.7, },
+		{ 0.6, 0.4, },
+		{ 1.0, 0.1, }
+	};
+
+	
+
+
+	/*double Fi[3][5];
 	double A[3][3];
 	double B[3]{ 0, 0, 0 };
 	double* C;
@@ -166,10 +195,55 @@ int main()
 		J = J + pow((Y[i]-Fun[i]),2);
 	
 	}
-	std::cout << J << "\t";
+	std::cout << J << "\t";*/
 
 
 	return 0;
 
 }
 
+//Р РµС€РµРЅРёРµ РЎР›РђРЈ РјРµС‚РѕРґРѕРј РёС‚РµСЂР°С†РёР№
+double* iter(double a[3][3], double y[3], int n)
+{
+	double* res = new double[n];
+	int i, j;
+
+
+	for (i = 0; i < n; i++)
+	{
+		res[i] = y[i] / a[i][i];
+	}
+
+	double eps = 0.0001;
+	double* Xn = new double[n];
+
+	do {
+		for (i = 0; i < n; i++) {
+			Xn[i] = y[i] / a[i][i];
+			for (j = 0; j < n; j++) {
+				if (i == j)
+					continue;
+				else {
+					Xn[i] -= a[i][j] / a[i][i] * res[j];
+				}
+			}
+		}
+
+		bool flag = true;
+		for (i = 0; i < n - 1; i++) {
+			if (abs(Xn[i] - res[i]) > eps) {
+				flag = false;
+				break;
+			}
+		}
+
+		for (i = 0; i < n; i++) {
+			res[i] = Xn[i];
+		}
+
+		if (flag)
+			break;
+	} while (1);
+
+	return res;
+}
