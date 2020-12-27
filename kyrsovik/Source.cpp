f982e44abfec1602a1c38deb047fcd8c2c3ea174
HoopeMax <nadezhda-maksimova-01@mail.ru>
2020-12-27 17:29:02 +0300

    переписала

--- kyrsovik/Source.cpp
+++ kyrsovik/Source.cpp
@@ -3,222 +3,217 @@
 #include <math.h>
 #include <initializer_list>
 
-const int QUANTITY_Y = 5;
-const int QUANTITY_X = 3;
+const int COLLS = 5;
+const int ROWS = 3;
 
-double* GaussMethod(double** Matrix);
-void AproksimFunction();
-class Matrix
+double* Itera(double** Matrix);
+void AproksimFun();
+
+class Mat
 {
-	double Y[QUANTITY_Y] = { 0.0f, 0.8f, 0.7f, 0.4f, 0.1f };
-	double CalcElement(unsigned int n, unsigned int m);
-	double CalcB(unsigned short n, double Y[QUANTITY_Y]);
+	double Y[COLLS] = { 0.0f, 0.8f, 0.7f, 0.4f, 0.1f };
+	double CreatElem(int n, int m);
+	double CalcB(int n, double Y[COLLS]);
 public:
-	double** mat;
-	Matrix(unsigned short height = 3, unsigned short width = 3)
+	double** mat_A;
+	Mat(int colls = 3, int rows = 3)
 	{
-		mat = new double* [height];
-		for (unsigned short step(0); step < width; step++)
+		mat_A = new double* [colls];
+		for (int step(0); step < rows; step++)
 		{
-			mat[step] = new double[width];
+			mat_A[step] = new double[rows];
 		}
-		FillMatrix();
+		FullMatA();
 	}
 
-	~Matrix()
+	~Mat()
 	{
-		delete[] mat;
+		delete[] mat_A;
 	}
-	void FillMatrix();
-	void Show();
+	void FullMatA();
+	void Show_MatA();
 };
 
-class BazF
+class BazisFunction
 {
-	double f1(double);
-	double f2(double);
-	double f3(double);
+	double fun1(double);
+	double fun2(double);
+	double fun3(double);
 public:
-	double** basic_f;
-	BazF(std::initializer_list<double> x_arr)
+	double** bas_x;
+	BazisFunction(std::initializer_list<double> x_arr)
 	{
-		basic_f = new double* [QUANTITY_Y];
+		bas_x = new double* [COLLS];
 
-		for (unsigned short step(0); step < QUANTITY_Y; step++)
+		for (int step(0); step < COLLS; step++)
 		{
-			basic_f[step] = new double[QUANTITY_X];
+			bas_x[step] = new double[ROWS];
 		}
-		for (unsigned short step_y(0); step_y < QUANTITY_Y; step_y++)
+		for (int step_y(0); step_y < COLLS; step_y++)
 		{
-			for (unsigned short step_x(0); step_x < QUANTITY_X; step_x++)
+			for (int step_x(0); step_x < ROWS; step_x++)
 			{
 				if (step_x == 0)
 				{
-					basic_f[step_y][step_x] = f1(*(x_arr.begin() + step_y));
+					bas_x[step_y][step_x] = fun1(*(x_arr.begin() + step_y));
 				}
 				else if (step_x == 1)
 				{
-					basic_f[step_y][step_x] = f2(*(x_arr.begin() + step_y));
+					bas_x[step_y][step_x] = fun2(*(x_arr.begin() + step_y));
 				}
 				else
 				{
-					basic_f[step_y][step_x] = f3(*(x_arr.begin() + step_y));
+					bas_x[step_y][step_x] = fun3(*(x_arr.begin() + step_y));
 				}
 			}
 		}
 	}
-	~BazF()
+	~BazisFunction()
 	{
-		delete[] basic_f;
+		delete[] bas_x;
 	}
 };
 
 int main()
 {
-	setlocale(LC_ALL, "russian");
-	AproksimFunction();
+	setlocale(LC_ALL, "rus");
+	AproksimFun();
 	system("pause");
 }
 
-double BazF::f1(double x)
+double BazisFunction::fun1(double x)
 {
 	return 1;
 }
 
-double BazF::f2(double x)
+double BazisFunction::fun2(double x)
 {
 	return x;
 }
 
-double BazF::f3(double x)
+double BazisFunction::fun3(double x)
 {
 	return 3*pow(x,2)-1;
 }
 
-double Matrix::CalcElement(unsigned int n, unsigned int m)
+double Mat::CreatElem(int n, int m)
 {
-	BazF basic_f({-1.0f,-0.3f,0.4f,0.6f,1.0f });
-	double result = 0;
-	for (unsigned short i(0); i < QUANTITY_Y; i++)
+	BazisFunction bas_f({-1.0f,-0.3f,0.4f,0.6f,1.0f });
+	double ansv = 0;
+	for (int i(0); i < COLLS; i++)
 	{
-		result += (basic_f.basic_f[i][n - 1] * basic_f.basic_f[i][m - 1]);
+		ansv += (bas_f.bas_x[i][n - 1] * bas_f.bas_x[i][m - 1]);
 	}
-	return result;
+	return ansv;
 }
 
-double Matrix::CalcB(unsigned short n, double Y[QUANTITY_Y])
+double Mat::CalcB(int n, double Y[COLLS])
 {
-	BazF basic_f({ -1.0f,-0.3f,0.4f,0.6f,1.0f });
-	double result = 0;
-	for (unsigned short i(0); i < QUANTITY_Y; i++)
+	BazisFunction basic_f({ -1.0f,-0.3f,0.4f,0.6f,1.0f });
+	double ansv = 0;
+	for (int i(0); i < COLLS; i++)
 	{
-		result += (Y[i] * basic_f.basic_f[i][n - 1]);
+		ansv += (Y[i] * basic_f.bas_x[i][n - 1]);
 	}
-	return result;
+	return ansv;
 }
 
-void Matrix::FillMatrix()
+void Mat::FullMatA()
 {
-	for (unsigned short i(0); i < 3; i++)
+	for (int i(0); i < 3; i++)
 	{
-		for (unsigned short j(0); j < 3; j++)
+		for (int j(0); j < 3; j++)
 		{
-			mat[i][j] = CalcElement(i + 1, j + 1);
+			mat_A[i][j] = CreatElem(i + 1, j + 1);
 		}
-		mat[i][3] = CalcB(i + 1, Y);
+		mat_A[i][3] = CalcB(i + 1, Y);
 	}
 }
 
-void Matrix::Show()
+void Mat::Show_MatA()
 {
-	for (unsigned short i(0); i < 3; i++)
+	for (int i(0); i < 3; i++)
 	{
-		for (unsigned short j(0); j < 4; j++)
+		for (int j(0); j < 4; j++)
 		{
-			std::cout << mat[i][j] << " ";
+			std::cout << mat_A[i][j] << " ";
 		}
 		std::cout << "\n";
 	}
 }
-double* GaussMethod(double** Matrix)
+
+double* Itera(double** MatA)
 {
 	double* result = new double[3];
-	for (unsigned short j(1); j < 3; j++)
+	for (int j(1); j < 3; j++)
 	{
-		double subdiv = Matrix[j][0] / Matrix[0][0];
-		for (unsigned short i(0); i < 4; i++)
+		double subdiv = MatA[j][0] / MatA[0][0];
+		for (int i(0); i < 4; i++)
 		{
-			Matrix[j][i] = Matrix[j][i] - (Matrix[0][i] * subdiv);
+			MatA[j][i] = MatA[j][i] - (MatA[0][i] * subdiv);
 		}
 	}
-	for (unsigned short j(2); j < 3; j++)
+	for (int j(2); j < 3; j++)
 	{
-		double subdiv = Matrix[j][1] / Matrix[1][1];
-		for (unsigned short i(1); i < 4; i++)
+		double subdiv = MatA[j][1] / MatA[1][1];
+		for (int i(1); i < 4; i++)
 		{
-			Matrix[j][i] = Matrix[j][i] - (Matrix[1][i] * subdiv);
+			MatA[j][i] = MatA[j][i] - (MatA[1][i] * subdiv);
 		}
 	}
 
-	result[2] = Matrix[2][3] / Matrix[2][2];
-	result[1] = (Matrix[1][3] - Matrix[1][2] * result[2]) / Matrix[1][1];
-	result[0] = (Matrix[0][3] - Matrix[0][1] * result[1] - Matrix[0][2] * result[2]) / Matrix[0][0];
+	result[2] = MatA[2][3] / MatA[2][2];
+	result[1] = (MatA[1][3] - MatA[1][2] * result[2]) / MatA[1][1];
+	result[0] = (MatA[0][3] - MatA[0][1] * result[1] - MatA[0][2] * result[2]) / MatA[0][0];
 	return result;
 }
-void AproksimFunction()
+
+void AproksimFun()
 {
-	double Y[QUANTITY_Y] = { 0.0f, 0.8f, 0.7f, 0.4f, 0.1f };
-	Matrix mat_object;
-	BazF basic_f({ -1.0f,-0.3f,0.4f,0.6f,1.0f });
+	double Y[COLLS] = { 0.0f, 0.8f, 0.7f, 0.4f, 0.1f };
+	Mat mat_A;
+	BazisFunction basic_f({ -1.0f,-0.3f,0.4f,0.6f,1.0f });
 	//Вывод изначальной матрицы
-	std::cout << "Изначальная матрица\n";
-	for (unsigned short i(0); i < 3; i++)
+	std::cout << "Матрица AC= B\n";
+	for (int i(0); i < 3; i++)
 	{
-		for (unsigned short j(0); j < 4; j++)
+		for (int j(0); j < 4; j++)
 		{
-			std::cout << mat_object.mat[i][j] << " ";
+			std::cout << mat_A.mat_A[i][j] << " ";
 		}
 		std::cout << std::endl;
 	}
-	double* answers = GaussMethod(mat_object.mat);
-	double f[QUANTITY_Y] = {};
-	double sigma[QUANTITY_Y] = {};
+	double* OutPut_C = Itera(mat_A.mat_A);
+	double f[COLLS] = {};
+	double sigm[COLLS] = {};
 	double J = 0;
-	for (unsigned short i(0); i < QUANTITY_Y; i++)
+	for (int i(0); i < COLLS; i++)
 	{
-		f[i] = *answers * basic_f.basic_f[i][0] + *(answers + 1) * basic_f.basic_f[i][1] + *(answers + 2) * basic_f.basic_f[i][2];
-		sigma[i] = abs(f[i] - Y[i]);
-		J += pow(sigma[i], 2);
+		f[i] = *OutPut_C * basic_f.bas_x[i][0] + *(OutPut_C + 1) * basic_f.bas_x[i][1] + *(OutPut_C + 2) * basic_f.bas_x[i][2];
+		sigm[i] = abs(f[i] - Y[i]);
+		J += pow(sigm[i], 2);
 	}
 
-	//Отсюда идет вывод данных
-	std::cout << "\nМатрица после обработки\n";
-	for (unsigned short i(0); i < 3; i++)
+	//Начало паботы метода
+
+	std::cout << "\n Метод простых итераций \n";
+	for (int i(0); i < 3; i++)
 	{
-		for (unsigned short j(0); j < 4; j++)
-		{
-			std::cout << mat_object.mat[i][j] << " ";
-		}
-		std::cout << std::endl;
-	}
-	std::cout << "\nX,Y и Z в результате выполнения метода Гаусса\n";
-	for (unsigned short i(0); i < 3; i++)
-	{
-		std::cout << *(answers + i) << " ";
+		std::cout << *(OutPut_C + i) << " ";
 	}
 	std::cout << "\n";
 	std::cout << "\nЗначения апрокс. функций\n";
-	for (unsigned short i(0); i < QUANTITY_Y; i++)
+	for (int i(0); i < COLLS; i++)
 	{
 		std::cout << f[i] << " ";
 	}
 	std::cout << "\n";
-	std::cout << "\nЗначения сигмы\n";
-	for (unsigned short i(0); i < QUANTITY_Y; i++)
+	std::cout << "\nЗначения фи\n";
+	for (int i(0); i < COLLS; i++)
 	{
-		std::cout << sigma[i] << " ";
+		std::cout << sigm[i] << " ";
 	}
 	std::cout << "\n";
 	std::cout << "\nJ = " << J << "\n";
-	delete[] answers;
+	delete[] OutPut_C;
 }
